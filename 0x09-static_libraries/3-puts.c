048ee3df8bc79375d0f3706e76992ca4416fe32d
Esseh12 <gbenmoese788@gmai.com>
2023-10-31 02:53:58 -0700

    redo commit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -4,6 +4,13 @@
 #include <ctype.h>
 #include <string.h>
 
+/**
+ * _puts - Prints a string to the standard output.
+ * @s: The string to be printed.
+ *
+ * Return: void
+ */
+
 void _puts(char *s)
 {
 puts(s);
